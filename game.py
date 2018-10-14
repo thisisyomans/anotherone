5257dfe146365ae4e44409d1f26cbef71b6e1317
thisisyomans <tanejamm@gmail.com>
2018-10-13 21:52:43 -0700

    started fps and quitting

--- game.py
+++ game.py
@@ -10,8 +10,21 @@ def resource_path(relative):
 	return os.path.join(relative)
 
 def main():
+	#initialize game
 	pygame.init()
+
+	#screen details
+	pygame.display.set_caption('RoboTirade')
+	width, height = 640, 480
+	screen = pygame.display.set_mode((width, height))
 	
+	#setting up fps
+	frame_count = 0
+    frame_rate = 0
+    t0 = time.clock()
+
+	#any needed vars, arrays, lists, etc
+	keys = [False, False, False, False]
 
 	#NOTE: image resources + small manipulations
 
@@ -22,6 +35,24 @@ def main():
 	exitcode = 0 #change num in game code and then call the next screen, exitcode will decide the next screen
 
 	while running:
+		
+		eventlook = pygame.event.poll()
+		
+		if eventlook.type == pygame.QUIT():
+			pygame.quit()
+		if eventlook.type == pygame.KEYDOWN:
+			if event.key == K_q:
+                pygame.quit()
+			
+
+
+		frame_count += 1
+        if frame_count % 500 == 0:
+            t1 = time.clock()
+            frame_rate = 500 / (t1-t0)
+            t0 = t1
+		
+		pygame.display.flip()
 	
 	if exitcode == 0:
 
