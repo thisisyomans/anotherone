94e0c10d4c9574b507b290c7a3edae141570c013
thisisyomans <tanejamm@gmail.com>
2018-10-20 11:15:49 -0700

    screen system working rn, total frames working, fps not working

--- game.py
+++ game.py
@@ -27,93 +27,84 @@ def main():
 	t0 = time.clock()
 
 	#any needed vars, arrays, lists, etc
+	restart = False
 	keys = [False, False, False, False]
+	running = 2 #better than just running a "while True:" loop, now we can check program state w/ a var
+	exitcode = 0 #change num in game code, set running to False, when running is set to False, while loop breaks, and game looks for next screen based on exitcode
+
+	#NOTE: possible restart function
+	#def restart_game():
+		#if(restart == True):
+			#restart = False
+			#change running state to switch scenes
+
+	#event system
+	def event_system():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			
+			if event.type == pygame.KEYDOWN:
+				if event.key == K_q:
+					pygame.quit()
+
+				if event.key == K_r and running == 3:
+					restart = True
+					restart_game()
+
+				if event.key == K_w:
+					keys[0] = True
+				if event.key == K_a:
+					keys[1] = True
+				if event.key == K_s:
+					keys[2] = True
+				if event.key == K_d:
+					keys[3] = True
+
+			if event.type == pygame.KEYUP:
+
+				if event.key == K_w:
+					keys[0] = False
+				if event.key == K_a:
+					keys[1] = False
+				if event.key == K_s:
+					keys[2] = False
+				if event.key == K_d:
+					keys[3] = False
 
 	#NOTE: image resources + small manipulations
 
 	#NOTE: sound resources + small manipulations
 
 	#NOTE: game itself
-	running = 1 #better than just running a "while True:" loop, now we can check program state w/ a var
-	exitcode = 0 #change num in game code, set running to False, when running is set to False, while loop breaks, and game looks for next screen based on exitcode
+	while running == 1 and restart == False: #NOTE: menu
+		#probably a clickable sprite button on screen for starting the game
+		#pygame.display.flip(), pretty sure I don't need it for now, but you never know
+		event_system() #I don't think I need the key system here, gonna have to check the logic
 
-	while running == 1: #NOTE: menu
-
-		eventlook = pygame.event.poll()
-		
-		if eventlook.type == pygame.QUIT():
-			pygame.quit()
-		
-		if eventlook.type == pygame.KEYDOWN:
-			if eventlook.key == K_q:
-				pygame.quit()
-
-		if eventlook.type == pygame.KEYUP:
-
-	while running == 2: #NOTE: game screen
-		
-		eventlook = pygame.event.poll()
-		
-		if eventlook.type == pygame.QUIT():
-			pygame.quit()
-		
-		if eventlook.type == pygame.KEYDOWN:
-			if eventlook.key == K_q:
-				pygame.quit()
-
-			if eventlook.key == K_w:
-				keys[0] = True
-			if eventlook.key == K_a:
-				keys[1] = True
-			if eventlook.key == K_s:
-				keys[2] = True
-			if eventlook.key == K_d:
-				keys[3] = True
-
-		if eventlook.type == pygame.KEYUP:
-			if eventlook.key == K_w:
-				keys[0] = False
-			if eventlook.key == K_a:
-				keys[1] = False
-			if eventlook.key == K_s:
-				keys[2] = False
-			if eventlook.key == K_d:
-				keys[3] = False
-
+	while running == 2 and restart == False: #NOTE: game screen
 		frame_count += 1
 		if frame_count % 500 == 0:
 			t1 = time.clock()
 			frame_rate = 500 / (t1-t0)
 			t0 = t1
+
+		screen.fill((255, 255, 255))
+		
+		my_font = pygame.font.Font(resource_path(os.path.join('resources', 'freesansbold.ttf')), 24)
+		the_text = my_font.render("Frame = {0},  rate = {1:.2f} fps"
+		.format(frame_count, frame_rate), True, (0,0,0))
+		screen.blit(the_text, (10, 10))
 		
 		pygame.display.flip()
-	
-	if exitcode == 0 and running == 3: #NOTE: losing end screen
-		
-		eventlook = pygame.event.poll()
-		
-		if eventlook.type == pygame.QUIT():
-			pygame.quit()
-		
-		if eventlook.type == pygame.KEYDOWN:
-			if eventlook.key == K_q:
-				pygame.quit()
-			if eventlook.key == K_r:
+		event_system()
 
-		if eventlook.type == pygame.KEYUP:
+	if exitcode == 0 and running == 3 and restart == False: #NOTE: losing end screen
+		#pygame.display.flip(), pretty sure I don't need it for now, but you never know
+		event_system()
 
-	if exitcode == 1 and running == 3: #NOTE: winning end screen
-
-		eventlook = pygame.event.poll()
-		
-		if eventlook.type == pygame.QUIT():
-			pygame.quit()
-		
-		if eventlook.type == pygame.KEYDOWN:
-			if eventlook.key == K_q:
-				pygame.quit()
-			if eventlook.key == K_r:
-
-		if eventlook.type == pygame.KEYUP:
+	if exitcode == 1 and running == 3 and restart == False: #NOTE: winning end screen
+		#pygame.display.flip(), pretty sure I don't need it for now, but you never know
+		event_system()
 
 main()
